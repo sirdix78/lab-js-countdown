e7c47e8e6ef2bfadd9d5a5374189b325df85b005
palaci spaci <kristi.gjoka92@gmail.com>
2025-02-27 15:45:37 +0100

    3 done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
-
+const timerElement = document.getElementById("time");
+const toastElement = document.getElementById("toast");
+const startBtn = document.getElementById('start-btn');
+const closeBtn = document.getElementById('close-toast');
+let toastTimeout = null;
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
-
+startBtn.addEventListener ('click', () => {
+  startCountdown();
+})
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
-  // Your code goes here ...
-}
-
-
-
+  startBtn.disabled = true;
+    timer = setInterval(() => {
+      remainingTime--;
+      timerElement.innerText = remainingTime;
+      if (remainingTime === 0) {  
+        clearInterval(timer);
+        showToast('Time is up');
+    }
+  }, 1000);
+};
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
+  toastElement.innerText = message;
+  toastElement.classList.add('show');
 
-  // Your code goes here ...
-
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+setTimeout(() => {
+  toastElement.classList.remove('show'); // Hide after 3s
+}, 3000);
 }
+
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+toastElement.addEventListener ('click', () => {
+  closeBtn.classList.remove('show');
+  clearTimeout(showToast);
+});
+
